138ed87aeb42824d36447809579e55fa2ae6afbc
nariomasahisa <nariomasahisa@gmail.com>
2022-08-19 00:21:02 +0900

    ポートフォリオ機能

---
+++ pages/portfolio/index.tsx
@@ -0,0 +1,11 @@
+const Portfolio = () => {
+    return(
+        <>
+            <div className="h-screen">
+                <h1>保有株一覧</h1>
+            </div>
+        </>
+    );
+}
+
+export default Portfolio;
