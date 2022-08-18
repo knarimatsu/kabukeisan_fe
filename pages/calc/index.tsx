efb7a2ae650ffd8d9e6f26d31e928b0cd2814162
nariomasahisa <nariomasahisa@gmail.com>
2022-08-19 00:07:06 +0900

    企業価値計算ページ

---
+++ pages/calc/index.tsx
@@ -0,0 +1,11 @@
+const Calc = () => {
+    return (
+        <>
+            <div className="h-screen">
+                <h1>企業価値計算</h1>
+            </div>
+        </>
+    );
+}
+
+export default Calc;
