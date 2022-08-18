6cefb93f2c74057b028be01dfa5fea3c4ba1a4bf
nariomasahisa <nariomasahisa@gmail.com>
2022-08-19 00:05:57 +0900

    部品追加

---
+++ components/Layout.tsx
@@ -0,0 +1,13 @@
+import { type } from "os";
+
+type AppLayoutProps = {
+    children: React.ReactNode;
+}
+
+export const Layout = ({children}:AppLayoutProps) => {
+    return(
+        <div>
+            {children}
+        </div>
+    )
+}
