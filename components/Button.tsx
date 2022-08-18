76919384c992ddf1ecaa71d84bbb61e20b10d007
nariomasahisa <nariomasahisa@gmail.com>
2022-08-19 00:06:35 +0900

    ボタン

---
+++ components/Button.tsx
@@ -0,0 +1,28 @@
+import Router, { useRouter } from "next/router";
+import { backgroundColor, borderColor, textColor } from "../libs/modules/bg-color";
+
+const Button = (
+    {
+        title,
+        color,
+        onClick
+    }: {
+        title:string
+        color: string
+        onClick: any
+    }
+) => {
+
+    return(
+        <>
+            <button
+                className={`${backgroundColor(color)} ${textColor(color)} ${borderColor(color)} text-lg px-4 py-2 rounded-lg m-4 `}
+                onClick={onClick}
+            >
+                {title}
+            </button>
+        </>
+    )
+}
+
+export default Button;
