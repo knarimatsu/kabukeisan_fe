0a12055a3c9eafabfff3aa46bf03945da8becb84
nariomasahisa <nariomasahisa@gmail.com>
2022-08-19 00:06:24 +0900

    メインページ

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Header from '../components/Header'
+import { Layout } from '../components/Layout'
+import Footer from '../components/Footer'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (
+    <>
+    <Header />
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+    <Footer/>
+    </>
+  )
 }
 
 export default MyApp
