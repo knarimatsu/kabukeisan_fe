43aad0d2be9189ba7b9618698d7e0300a80d9cda
nariomasahisa <nariomasahisa@gmail.com>
2022-08-19 00:05:38 +0900

    tailwind.css導入

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    // colors: {
+    //   "primary": "#25D366",
+    //   "black": "#131418",
+    //   "red": "#BD081C",
+    //   "blue": "#0099FF",
+    //   "white": "#ffffff"
+    // },
+    extend: {},
+  },
+  plugins: [],
+}
