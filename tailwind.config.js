86eb2169498e4059b7df098af19227a772bc5bd5
nariomasahisa <nariomasahisa@gmail.com>
2022-08-24 21:28:23 +0900

    恩株機能のデザイン

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    // colors: {
-    //   "primary": "#25D366",
-    //   "black": "#131418",
-    //   "red": "#BD081C",
-    //   "blue": "#0099FF",
-    //   "white": "#ffffff"
-    // },
     extend: {},
   },
   plugins: [],
-}
+};
