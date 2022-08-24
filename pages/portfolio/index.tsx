2001816f2ecd748b4763b0838a559782d86a30e8
nariomasahisa <nariomasahisa@gmail.com>
2022-08-24 23:20:14 +0900

    ポートフォリオデザイン

--- pages/portfolio/index.tsx
+++ pages/portfolio/index.tsx
@@ -1,10 +1,15 @@
+import SummaryCard from "../../components/SummaryCard";
+
 const Portfolio = () => {
     return(
-        <>
-            <div className="h-screen">
-                <h1>保有株一覧</h1>
-            </div>
-        </>
+        <main className="h-screen">
+
+            <h1 className="text-2xl m-3">保有株一覧</h1>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="fill-[#25D366] text-white w-9 h-9 fixed right-0 top-[60px] right-5">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+            <SummaryCard />
+        </main>
     );
 };
 
