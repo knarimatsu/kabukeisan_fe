408f3446729671adf457ec8a97063bc5d08b0a88
nariomasahisa <nariomasahisa@gmail.com>
2022-12-10 23:52:25 +0900

    upgrade next version

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    target: "serverless",
     reactStrictMode: true,
     swcMinify: true,
-    future: {
-        webpack5: true,
-    },
 };
 
 module.exports = nextConfig;
