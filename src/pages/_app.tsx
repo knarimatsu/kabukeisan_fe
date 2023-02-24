f587515f84738c7644a20d3cabeaa942b08da8ca
nariomasahisa <nariomasahisa@gmail.com>
2023-02-25 02:06:52 +0900

    fix root component to add recoil root

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,16 +4,19 @@ import type { AppProps } from "next/app";
 import Header from "../components/Header";
 import { Layout } from "../components/Layout";
 import Footer from "../components/Footer";
+import { RecoilRoot } from "recoil";
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <SessionProvider>
-            <Header />
-            <Layout>
-                <Component {...pageProps} />
-            </Layout>
-            <Footer />
-        </SessionProvider>
+        <RecoilRoot>
+            <SessionProvider>
+                <Header />
+                <Layout>
+                    <Component {...pageProps} />
+                </Layout>
+                <Footer />
+            </SessionProvider>
+        </RecoilRoot>
     );
 }
 
