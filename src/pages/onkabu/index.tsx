0157058d6e612effa68fcc5cbf4774191c6c81f1
nariomasahisa <nariomasahisa@gmail.com>
2023-02-25 02:19:57 +0900

    fix calc onkabu component to change calculation

--- src/pages/onkabu/index.tsx
+++ src/pages/onkabu/index.tsx
@@ -2,22 +2,34 @@ import { useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
 import { CalcData } from "../../libs/client/interfaces/calc-data";
 import { calcOnkabu } from "../api/onkabu";
+import { useRecoilState } from "recoil";
+import { onkabuResultState } from "../../libs/recoil/atom";
 
 const Onkabu = () => {
     const { register, handleSubmit } = useForm<CalcData>({
         mode: "onSubmit",
     });
-    const post = (data: CalcData) => {
-        console.log(data);
-        // calcOnkabu(data);
+    const [onkabuResult, setOnkabuResult] = useRecoilState(onkabuResultState);
+    const calcOnkabu = (data: CalcData): void => {
+        const buy = Number(data.buyPrice);
+        const now = Number(data.nowPrice);
+        const stock = Number(data.stock);
+        const onkabu = Math.round(
+            stock - (((now - buy) * 0.79685) / now) * stock
+        );
+
+        if (onkabu < stock) {
+            setOnkabuResult(onkabu + "株売却してください");
+        } else {
+            setOnkabuResult("恩株は作れません");
+        }
     };
-    const { t } = useTranslation();
     return (
         <main className="h-screen">
             <h1 className="text-2xl m-3">{"Onkabu"}</h1>
             <form
                 className="px-10 py-8 w-3/4 my-4 mx-auto border rounded-lg"
-                onSubmit={handleSubmit(post)}
+                onSubmit={handleSubmit(calcOnkabu)}
             >
                 <label htmlFor="stock" className="block my-5">
                     株数
@@ -50,6 +62,7 @@ const Onkabu = () => {
                     <input type="submit" className="border py-1 w-full" />
                 </div>
             </form>
+            <h2 className="mx-auto mt-3">{onkabuResult}</h2>
         </main>
     );
 };
