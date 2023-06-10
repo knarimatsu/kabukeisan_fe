0ee5853c983516bb1348af0ae5975a5f8442686c
nariomasahisa <nariomasahisa@gmail.com>
2023-06-11 01:48:00 +0900

    feat: add authentication

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,38 +9,39 @@ import "../../src/i18n/config";
 import Head from "next/head";
 import { useTranslation } from "react-i18next";
 import Script from "next/script";
+import type { Session } from "next-auth";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps<{ session: Session }>) {
     const { t } = useTranslation();
     return (
         <>
-            <Head>
-                <title>{t("header.title")}</title>
-                <link rel="icon" href="../public/favicons/favicon.ico" />
-                <link rel="shortcut icon" href="/favicon.ico" />
-            </Head>
-            <Script
-                src={`https://www.googletagmanager.com/gtm.js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
-                strategy="afterInteractive"
-            />
-            <Script id="gtm-script" strategy="afterInteractive">
-                {`
+            <SessionProvider session={pageProps.session}>
+                <Head>
+                    <title>{t("header.title")}</title>
+                    <link rel="icon" href="../public/favicons/favicon.ico" />
+                    <link rel="shortcut icon" href="/favicon.ico" />
+                </Head>
+                <Script
+                    src={`https://www.googletagmanager.com/gtm.js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+                    strategy="afterInteractive"
+                />
+                <Script id="gtm-script" strategy="afterInteractive">
+                    {`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
 
                     gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
                 `}
-            </Script>
-            <RecoilRoot>
-                <SessionProvider>
+                </Script>
+                <RecoilRoot>
                     <Header />
                     <Layout>
                         <Component {...pageProps} />
                     </Layout>
                     <Footer />
-                </SessionProvider>
-            </RecoilRoot>
+                </RecoilRoot>
+            </SessionProvider>
         </>
     );
 }
