3c5b1a4e709231fe98939c4f9153694e222a1abe
nariomasahisa <nariomasahisa@gmail.com>
2023-06-11 15:42:14 +0900

    fix: test env data

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,11 +4,14 @@ import GoogleProvider from "next-auth/providers/google";
 export const authOptions: NextAuthOptions = {
     providers: [
         GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+            clientId:
+                "269946283050-9j09u2vopedd5uof6c0kukd38duqjbop.apps.googleusercontent.com",
+            // clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: "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",
+            // clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         }),
     ],
-    secret: process.env.NEXTAUTH_SECRET,
+    secret: "EUQdZIj6UzmTk7qZiGWtlM7zRFZTxbl6c69qeBG6D3A=",
 };
 
 export default NextAuth(authOptions);
