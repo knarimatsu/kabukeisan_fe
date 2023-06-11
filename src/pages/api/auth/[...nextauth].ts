3ce5d812d7ca09b22e7132c8edb3082b6e58fa3e
nariomasahisa <nariomasahisa@gmail.com>
2023-06-11 22:13:06 +0900

    chore: test

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,11 +4,14 @@ import GoogleProvider from "next-auth/providers/google";
 export const authOptions: NextAuthOptions = {
     providers: [
         GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+            clientId:
+                "249061083282-6rbd5b8dnjs928rak179u5b7qc6jcrvj.apps.googleusercontent.com",
+            clientSecret: "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",
+            // clientId: process.env.GOOGLE_CLIENT_ID as string,
+            // clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         }),
     ],
-    secret: process.env.NEXTAUTH_SECRET as string,
+    // secret: process.env.NEXTAUTH_SECRET as string,
 };
 
 export default NextAuth(authOptions);
