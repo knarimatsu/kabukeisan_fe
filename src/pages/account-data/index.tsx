1f8b3c9f31f178465207638ccd01ce140afc99da
nariomasahisa <nariomasahisa@gmail.com>
2023-06-12 00:41:30 +0900

    fix: add UnderConstruction Component

--- src/pages/account-data/index.tsx
+++ src/pages/account-data/index.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
-import { useRecoilState } from "recoil";
 
-import { calcValueState } from "../../libs/recoil/atom";
-import { calcCompanyValue } from "../../libs/service/calc-value";
 import axios from "axios";
+import { useSession, signIn } from "next-auth/react";
+import UnderConstruction from "../../components/UnderConstruction";
 
 type RequestForm = {
     code: string;
@@ -15,6 +14,7 @@ const AccountData = () => {
         mode: "onSubmit",
     });
     const { t } = useTranslation();
+    const { data: session } = useSession();
 
     const getAccountData = async (data: RequestForm) => {
         axios
@@ -33,30 +33,46 @@ const AccountData = () => {
             });
     };
     return (
-        <main className="h-screen">
-            <h1 className="text-2xl m-3">{t("index.calcTitle")}</h1>
-            <form
-                className="px-10 py-8 w-3/4 my-4 mx-auto border rounded-lg"
-                onSubmit={handleSubmit(getAccountData)}
-            >
-                <label htmlFor="buy-price" className="block my-5">
-                    {t("calcValue.marketCap")}
-                    <input
-                        id="buy-price"
-                        {...register("code")}
-                        type="text"
-                        className="block border w-full h-9 outline-none p-3 rounded-md"
-                    />
-                </label>
-                <button
-                    className="mx-auto bg-blue-500 hover:bg-blue-700
-                    text-white font-bold py-2 px-4 rounded"
-                    type="submit"
-                >
-                    {t("index.calcTitle")}
-                </button>
-            </form>
-        </main>
+        <>
+            <UnderConstruction />
+        </>
+
+        // <main className="h-screen">
+        //     {session && (
+        //         <div>
+        //             <h1 className="text-2xl m-3">{t("index.calcTitle")}</h1>
+        //             <form
+        //                 className="px-10 py-8 w-3/4 my-4 mx-auto border rounded-lg"
+        //                 onSubmit={handleSubmit(getAccountData)}
+        //             >
+        //                 <label htmlFor="buy-price" className="block my-5">
+        //                     {t("calcValue.marketCap")}
+        //                     <input
+        //                         id="buy-price"
+        //                         {...register("code")}
+        //                         type="text"
+        //                         className="block border w-full h-9 outline-none p-3 rounded-md"
+        //                     />
+        //                 </label>
+        //                 <button
+        //                     className="mx-auto bg-blue-500 hover:bg-blue-700
+        //             text-white font-bold py-2 px-4 rounded"
+        //                     type="submit"
+        //                 >
+        //                     {t("index.calcTitle")}
+        //                 </button>
+        //             </form>
+        //         </div>
+        //     )}
+        //     {!session && (
+        //         <div>
+        //             <p>
+        //                 財務データ参照機能を利用したい場合はログインしてください
+        //             </p>
+        //             <button onClick={() => signIn()}>Sign in</button>
+        //         </div>
+        //     )}
+        // </main>
     );
 };
 
