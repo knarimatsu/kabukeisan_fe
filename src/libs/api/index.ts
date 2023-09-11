12ac98f3ec993cba8e54276d800b66438f5eca50
knarimatsu <nariomasahisa@gmail.com>
2023-09-11 20:53:31 +0900

    fix: api directory

---
+++ src/libs/api/index.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+export const getAPI = async (url: string, request: any) => {
+    try {
+        const res = await axios.post(url, request);
+        return res;
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+export const createUser = async (url: string, data: any) => {
+    const request = {
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": "application/json",
+        },
+        withCredentials: true,
+        body: data,
+    };
+    try {
+        const res = await axios.post(url, request);
+        return res;
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+const deleteUser = async (data: any) => {};
+
+const updateUser = async (data: any) => {};
