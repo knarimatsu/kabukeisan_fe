8138233efbd420accf37a0c707d6866ccb324857
knarimatsu <nariomasahisa@gmail.com>
2023-10-03 22:11:30 +0900

    fix: 送信ボタンを変更

--- src/pages/check-stock/index.tsx
+++ src/pages/check-stock/index.tsx
@@ -3,11 +3,14 @@ import React from "react";
 import dynamic from "next/dynamic";
 import RadioButton from "../../components/RadioButton";
 import { CHECKSTOCKCONTENT } from "../../utils/CheckStockContent";
+import { Button, Input } from "@mui/material";
+import { useTranslation } from "react-i18next";
 
 const Header = dynamic(() => import("../../components/Header"));
 const Footer = dynamic(() => import("../../components/Footer"));
 
 const CheckStock = () => {
+    const { t } = useTranslation();
     const { register, handleSubmit, reset } = useForm();
     const onSubmit = (data: any) => {
         console.log(data);
@@ -20,14 +23,25 @@ const CheckStock = () => {
                 onSubmit={handleSubmit(onSubmit)}
             >
                 {CHECKSTOCKCONTENT.map((content, key) => (
-                    <div key={key}>
+                    <div
+                        key={key}
+                        className="w-1/2 px-6 py-2 mx-auto my-2 border border-white rounded-md dark:border-gray-400 dark:bg-gray-800"
+                    >
                         <RadioButton
                             RadioButtonContent={content}
                             register={register}
                         />
                     </div>
                 ))}
-                <input type="submit" />
+                <div className="text-center my-8">
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        className="bg-[#2e7d32] hover:bg-[#1b5e20]"
+                    >
+                        {t("button.send")}
+                    </Button>
+                </div>
             </form>
             <Footer />
         </>
