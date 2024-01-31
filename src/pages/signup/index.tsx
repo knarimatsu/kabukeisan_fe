1c3195ca8c7f303bcd22bade0f4f769f6d513bf8
knarimatsu <nariomasahisa@gmail.com>
2024-01-31 23:47:37 +0900

    apiディレクトリの移動

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { Button, TextField } from "@mui/material";
-import { createUser } from "../../app/libs/api";
+import { createUser } from "../../app/api";
 
 type InputUserInfo = {
     nickname: string;
