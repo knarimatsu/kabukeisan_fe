1b079e01bdf16ec8d9f8195e903ea37dcff54c9a
knarimatsu <nariomasahisa@gmail.com>
2024-04-10 09:12:58 +0900

    ログインページ作成

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,11 +6,8 @@ import {
     CognitoUser,
 } from "amazon-cognito-identity-js";
 import { useForm } from "react-hook-form";
+import { UserData } from "../../types/UserData";
 
-type UserData = {
-    username: string;
-    password: string;
-};
 type PoolData = {
     UserPoolId: string;
     ClientId: string;
@@ -21,21 +18,20 @@ const Login = () => {
         UserPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID as string,
         ClientId: process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID as string,
     };
-    console.log(poolData);
     const userPool = new CognitoUserPool(poolData);
     const { register, handleSubmit } = useForm<UserData>({
         mode: "onSubmit",
     });
     const login = (data: UserData): void => {
         const authenticationData = {
-            Username: data.username,
+            Username: data.email,
             Password: data.password,
         };
         const authenticationDetails = new AuthenticationDetails(
             authenticationData
         );
         const userData = {
-            Username: data.username,
+            Username: data.email,
             Pool: userPool,
         };
         const cognitoUser = new CognitoUser(userData);
@@ -60,12 +56,12 @@ const Login = () => {
                 className="w-96 px-10 py-8 my-4 mx-auto border rounded-lg dark:bg-gray-900"
                 onSubmit={handleSubmit(login)}
             >
-                <label htmlFor="username" className="block my-5">
+                <label htmlFor="email" className="block my-5">
                     {"取得株数"}
                     <input
-                        id="username"
+                        id="email"
                         type="text"
-                        {...register("username")}
+                        {...register("email")}
                         className="block border w-full h-9 outline-none p-3 rounded-md dark:bg-gray-700"
                     />
                 </label>
