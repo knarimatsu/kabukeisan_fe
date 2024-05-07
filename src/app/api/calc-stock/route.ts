10a46fb13be1dab6634d609e86e047f4a5c8e8af
knarimatsu <nariomasahisa@gmail.com>
2024-05-07 23:25:33 +0900

    fix: cals-stock api

--- src/app/api/calc-stock/route.ts
+++ src/app/api/calc-stock/route.ts
@@ -1,25 +1,28 @@
 import { NextRequest, NextResponse } from "next/server";
 import { calcCompanyValue } from "../../libs/service/calc-value";
-import { PostData } from "../../../types/post-data";
+import { PostData, ScouterData } from "../../../types/post-data";
 
 export async function GET(req: NextRequest) {
     const searchParams = await req.nextUrl.searchParams;
-    const buyPrice = Number(searchParams.get("buyPrice"));
-    const profit = Number(searchParams.get("profit"));
-    const depreciation = Number(searchParams.get("depreciation"));
-    const investing = Number(searchParams.get("investing"));
-    const roic = Number(searchParams.get("roic"));
-    const cash = Number(searchParams.get("cash"));
-    const equity = Number(searchParams.get("equity"));
-    const data: PostData = {
-        buyPrice,
-        profit,
-        depreciation,
-        investing,
-        roic,
-        cash,
-        equity,
+    const isShikiho = searchParams.get("isShikiho") === "true";
+    const shikihoData: PostData = {
+        buyPrice: Number(searchParams.get("buyPrice")),
+        profit: Number(searchParams.get("profit")),
+        depreciation: Number(searchParams.get("depreciation")),
+        investing: Number(searchParams.get("investing")),
+        roic: Number(searchParams.get("roic")),
+        cash: Number(searchParams.get("cash")),
+        equity: Number(searchParams.get("equity")),
     };
+    const scouterData: ScouterData = {
+        ...shikihoData,
+        currentAsset: Number(searchParams.get("currentAsset")),
+        debt: Number(searchParams.get("debt")),
+        securities: Number(searchParams.get("securities")),
+    };
+
+    const data = isShikiho ? shikihoData : scouterData;
+
     const result = await calcCompanyValue(data);
     return NextResponse.json({
         status: 200,
